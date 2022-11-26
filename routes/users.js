6f1fd37a343e615ca823894a60e29898e87a8b4c
mayank <89704806+scanfMyName@users.noreply.github.com>
2022-11-26 20:32:11 +0530

    .gitignore is now working

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,4 @@ const user_controller = require("../middleware/user_controller");
 
 router.route("/register").post( user_controller.userRegister);
 router.route("/login").post(user_controller.userLogin);
-
-
-
 module.exports = router;
